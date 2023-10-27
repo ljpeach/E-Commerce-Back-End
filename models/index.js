7ad1db28e874986c09a74ccc186ec907ac3c1c19
Liam Peachey <liamjpeachey@gmail.com>
2023-10-27 13:27:42 -0500

    Completed product routes

--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'tag_by_product'
+  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_by_tag'
+  as: 'tagged_products'
 });
 
 module.exports = {
